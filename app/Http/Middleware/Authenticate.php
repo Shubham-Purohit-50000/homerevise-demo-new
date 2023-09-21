c3eae04c214580c927d8e14fadcdc83c173de1d6
Shubham-Purohit-50000 <117735266+ShubhamSolutions1313@users.noreply.github.com>
2023-09-21 17:01:32 +0530

    major changes on 21 sept

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Log;
 
 class Authenticate extends Middleware
 {
@@ -15,7 +16,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return redirect('/');
+            return route('api.login');
         }
     }
 }
